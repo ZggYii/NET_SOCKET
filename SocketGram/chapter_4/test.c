58b67cec687fc729fa79a3b1f8ebc033acce6687
ZggYii <zggyii0615@.gmail.com>
2023-07-26 11:27:29 +0800

    new file:   SocketGram/.tcp_client.c.swp 	new file:   SocketGram/chapter_10/echo_ser_pid/echo_mpserv 	new file:   SocketGram/chapter_10/echo_ser_pid/echo_mpserv.c 	new file:   SocketGram/chapter_10/signal 	new file:   SocketGram/chapter_10/signal.c 	new file:   SocketGram/chapter_10/wait 	new file:   SocketGram/chapter_10/wait.c 	new file:   SocketGram/chapter_10/waitpid 	new file:   SocketGram/chapter_10/waitpid.c 	new file:   SocketGram/chapter_10/zgram 	new file:   SocketGram/chapter_10/zombie.c 	new file:   SocketGram/chapter_11/echo_client.c 	new file:   SocketGram/chapter_11/echo_clnt 	new file:   SocketGram/chapter_11/echo_storeserv.c 	new file:   SocketGram/chapter_11/echomsg.txt 	new file:   SocketGram/chapter_11/storeserv 	new file:   SocketGram/chapter_12/client 	new file:   SocketGram/chapter_12/echo_client.c 	new file:   SocketGram/chapter_12/echo_selectserv.c 	new file:   SocketGram/chapter_12/selectserv 	new file:   SocketGram/chapter_18/cclnt 	new file:   SocketGram/chapter_18/chat_clnt.c 	new file:   SocketGram/chapter_18/chat_server.c 	new file:   SocketGram/chapter_18/cserv 	new file:   SocketGram/chapter_18/semaphore.c 	new file:   SocketGram/chapter_4/echo_client 	new file:   SocketGram/chapter_4/echo_client.c 	new file:   SocketGram/chapter_4/echo_server 	new file:   SocketGram/chapter_4/echo_server.c 	new file:   SocketGram/chapter_4/test.c 	new file:   SocketGram/chapter_4/test_gram 	new file:   SocketGram/chapter_4/testt 	new file:   SocketGram/chapter_6/uecho_client 	new file:   SocketGram/chapter_6/uecho_client.c 	new file:   SocketGram/chapter_6/uecho_server 	new file:   SocketGram/chapter_6/uecho_server.c 	new file:   SocketGram/chapter_8/gethost 	new file:   SocketGram/chapter_8/gethostbyname.c 	new file:   SocketGram/file_test/data.txt 	new file:   SocketGram/file_test/lopen 	new file:   SocketGram/file_test/low_open.c 	new file:   SocketGram/file_test/low_read.c 	new file:   SocketGram/file_test/lread 	new file:   SocketGram/hclient 	new file:   SocketGram/hclnt 	new file:   SocketGram/hello_client.c 	new file:   SocketGram/hello_server.c 	new file:   SocketGram/hserver 	new file:   SocketGram/tcp_clnt.c 	new file:   SocketGram/tcp_server.c 	new file:   SocketGram/test.c 	new file:   SocketGram/test/aton 	new file:   SocketGram/test/inet_aton.c

---
+++ SocketGram/chapter_4/test.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    fputs("This is a message.You are my destiny\n", stdout);
+    return 0;
+}
---
+++ SocketGram/test.c
@@ -0,0 +1,9 @@
+
+
+int main()
+{
+    int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in servaddr;
+    servaddr.
+}
